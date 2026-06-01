abddbf6339483400e05b964dbb0bed5eeb124bee
Alvaro <alvarodam87@gmail.com>
2026-06-01 20:05:27 +0200

    "Ejercicio 2: lista, búsqueda y fichero

---
+++ VideoTech/Pelicula.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoTech
+{
+    internal class Pelicula
+    {
+        private string titulo;
+        private string director;
+        private int anyo;
+        private bool disponible;
+
+        //constructor con parametros
+        public Pelicula(string titulo, string director, int anyo)
+        {
+            this.titulo = titulo;
+            this.director = director;
+            this.anyo = anyo;
+            this.disponible = true; // por defecto, la película está disponibles
+        }
+
+        //metodos get y set
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public string Director
+        {
+            get { return director; }
+            set { director = value; }
+        }
+        public int Anyo
+        {
+            get { return anyo; }
+            set { anyo = value; }
+        }
+
+        public bool Disponible
+        {
+            get { return disponible; }
+            set { disponible = value; }
+        }
+        //metodo ToString() devuelvva titulo, director y año de la película
+        public override string ToString()
+        {
+            return $"Título: {titulo}, Director: {director}, Año: {anyo}, Disponible: {disponible}";
+        }
+
+    }
+}
--- VideoTech/Program.cs
+++ VideoTech/Program.cs
@@ -1,1 +1,62 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+
+namespace VideoTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear una lista de peliculas con Add()
+            List<Pelicula> peliculas = new List<Pelicula>();
+            peliculas.Add(new Pelicula("El Padrino", "Francis Ford Coppola", 1972));
+            peliculas.Add(new Pelicula("El Padrino II", "Francis Ford Coppola", 1974));
+            peliculas.Add(new Pelicula("El Padrino III", "Francis Ford Coppola", 1990));
+
+            // Muestra por consola todas las películas con un foreach y ToString()
+            foreach (Pelicula p in peliculas)
+            {
+                Console.WriteLine(p.ToString());
+            }
+            Console.WriteLine();
+
+            //  Recorre la lista y muestra solo las películas cuyo director contenga el texto "Nolan". Usa Contains() de String
+            Console.WriteLine("Películas con director que contiene 'Nolan':");
+            foreach (Pelicula p in peliculas)
+            {
+                if (p.Director.Contains("Nolan"))
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+
+            //Muestra por consola la fecha actual en formato corto. Usa DateTime.Now y ToShortDateString().
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+
+            //Crear método GuardarPeliculas(List<Pelicula> lista, string ruta) que guarde todas las películas en un fichero de texto, con cada película en una línea y los campos separados por;.
+           public void GuardarPeliculas(List<Pelicula> lista, string ruta)
+            {
+                using (StreamWriter sw = new StreamWriter(ruta))
+                {
+                    foreach (Pelicula p in lista)
+                    {
+                        sw.WriteLine($"{p.Titulo};{p.Director};{p.Anyo};{p.Disponible}");
+                    }
+                }
+                
+
+
+
+
+
+
+
+            }
+        
+    }
+}
